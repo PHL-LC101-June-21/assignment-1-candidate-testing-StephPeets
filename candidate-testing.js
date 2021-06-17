e30f8d762c1d2dd201745e0ffabbb5f07cf2a078
Steph <84749465+StephPeets@users.noreply.github.com>
2021-06-17 07:04:36 +0000

    Added questions and correctAnswers arrays, added  askQuestion() function

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,40 +6,52 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let a;
 let question;
 let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let candidateAnswer = [];
+let candidateAnswers = 0;
+let questions = [
+/*0*/	"Who was the first American woman in space?", 
+/*1*/	"True or false: 5 kilometer == 5000 meters?", 
+/*2*/	"(5 + 3)/2 * 10 = ?", 
+/*3*/	"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 
+/*4*/	"What is the minimum crew size for the ISS?"];
+let correctAnswers = [
+/*0*/ 	"Sally Ride",
+/*1*/ 	"true",
+/*2*/ 	"40",
+/*3*/ 	"Trajectory",
+/*4*/ 	"3"];
 
 
+	
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 	candidateName = input.question("What is your name?\n")
 }
 
-question = "Who was the first American woman in space? ";
-correctAnswer = "Sally Ride"
-candidateAnswer = "";
-
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-	candidateAnswer = input.question(question);
+	for (a = 0; a < questions.length; a++) {
+		candidateAnswer[a] = input.question(questions[a]);
+		// candidateAnswer[a] = String(candidateAnswer);
+			if (String(candidateAnswer[a]).toLowerCase() == correctAnswers[a].toLowerCase()) {
+			candidateAnswers++;
+			console.log(`${correctAnswers[a]} is correct!\n`)
+		} else {
+			console.log(`Unfortunately, ${candidateAnswer[a]} is wrong. The correct answer is ${correctAnswers[a]}.\n`)
+		}
+	}
+console.log(candidateAnswer);
+console.log(candidateAnswers);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-	candidateAnswer = String(candidateAnswer);
-	if (candidateAnswer.toLowerCase() == correctAnswer.toLowerCase()) {
-		console.log(`Correct!`)
-	} else {
-		console.log(`Sorry, the correct answer is ${correctAnswer}.`)
-	}
 
-  let grade;
-  
+  let grade; // = candidateAnswers/correctAnswers.length*100;
 
   return grade;
 }
@@ -47,7 +59,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log(`Hi, ${candidateName}.`)
+  console.log(`Hi, ${candidateName}. It's Quiz Time!`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
