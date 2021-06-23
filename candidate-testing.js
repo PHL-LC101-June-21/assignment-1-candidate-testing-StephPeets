d66fd39298d8132f9779e13daa9bcadae61cdf31
Steph <84749465+StephPeets@users.noreply.github.com>
2021-06-23 03:50:51 +0000

    experimenting with "return". Will keep change to askQuestion (line 37)

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,34 +1,43 @@
 const input = require('readline-sync');
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+
 let candidateName = "";
-let candidateAnswers = [];
-let candidateAnswer = "";
+let question = "Who was the first American woman in space? ";
 let questions = [
   "Who was the first American woman in space? ", 
   "True or false: 5 kilometer == 5000 meters? ",
   "(5 + 3)/2 * 10 = ? ", 
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
   "What is the minimum crew size for the ISS? "];
+let correctAnswer = "Sally Ride";
 let correctAnswers = [
   "Sally Ride",
   "true",
   "40",
   "Trajectory",
   "3"];
+let candidateAnswer = "";
+let candidateAnswers = [];
+let consoleTest = "";
 
+// = `Correct Answer: ${correctAnswers[i]}\n`;
 
 function askForName() {
 	candidateName = input.question("Candidate Name: ")
-	console.log(`Hi, ${candidateName}. It's Quiz Time!\n`)
+	return console.log(`Hi, ${candidateName}. It's Quiz Time!\n`);
 }
+
+
+// 10.4.1.1. Returning a Value states:
+	// Now that we have return statements in our coding toolbox, we will very rarely print anything within a function. If we want to see the value returned by a function then we must print it after calling the function.
+
+/***	Is it possible to do this assignment without using console.log?	***/
 
 function askQuestion() {
 	for (let i = 0; i < questions.length; i++) {
-		console.log(questions[i])
-		candidateAnswers[i] = input.question(`Your Answer: `);
-    console.log(`Correct Answer: ${correctAnswers[i]}\n`)
+		candidateAnswers[i] = input.question(`${questions[i]}\nYour Answer: `);
+		console.log(`Correct Answer: ${correctAnswers[i]}\n`);
 	}
+	return;
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -38,8 +47,8 @@ function gradeQuiz(candidateAnswers) {
       grade++;
 		}
   }
-	candidateAnswer = grade.toString();
- 	grade = grade/questions.length*100;  
+	candidateAnswer = grade;
+ 	grade = grade/correctAnswers.length*100;
   console.log(`>>> Overall Grade: ${grade}% (${candidateAnswer} of 5 responses correct) <<<`)
   if (grade >= 80) {
     console.log(`>>> Status: PASSED <<<`)
